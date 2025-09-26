d37d29568f799f11ecc7081e60c7878b057c505b
rajeshakula <rajav231@gmail.com>
2025-09-26 22:36:11 +0100

    Delete companies-house-test-challenge-master/companies-house-test-challenge-master directory

--- companies-house-test-challenge-master/companies-house-test-challenge-master/cypress/support/step_definitions/navigation.steps.js
+++
@@ -1,5 +0,0 @@
-import { Then } from '@badeball/cypress-cucumber-preprocessor';
-
-Then('I should see product {string} in the gallery', (productName) => {
-  cy.contains('.card-title a', productName, { timeout: 10000 }).should('be.visible');
-});
